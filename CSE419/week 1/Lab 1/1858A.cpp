8ec566f90ccce3db406f888b81113d41b56372bd
Khaled Karim <kskarim177@gmail.com>
2024-06-06 02:22:56 +0600

    that facking long long int

---
+++ CSE419/week 1/Lab 1/1858A.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        int sum = a + b + c;
+        if (a == b)
+        {
+            if (c % 2 == 0)
+            {
+                cout << "Second" << endl;
+            }
+            else
+            {
+                cout << "First" << endl;
+            }
+        }
+        else
+        {
+            if (sum - a > sum - b)
+            {
+                cout << "Second" << endl;
+            }
+            else
+            {
+                cout << "First" << endl;
+            }
+        }
+    }
+    return 0;
+}
