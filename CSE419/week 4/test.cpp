1e91f8c0d555ea7a1a5f03835f0b4b16cbf6a149
Khaled Karim <kskarim177@gmail.com>
2024-07-06 17:10:11 +0600

    J - Reverse a substring

---
+++ CSE419/week 4/test.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+#ifdef _DEBUG
+    freopen("input.txt", "r", stdin);
+//	freopen("output.txt", "w", stdout);
+#endif
+    if ('a' > 'b')
+    {
+        cout << "a is less than b";
+    }
+    return 0;
+}
