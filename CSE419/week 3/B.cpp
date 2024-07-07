d9fdf71b9a4bba6714b4932385228f524a59fd96
Khaled Karim <kskarim177@gmail.com>
2024-07-08 02:54:17 +0600

    B - Apartment(CSES) uf the satisfaction...grrrr

--- CSE419/week 3/B.cpp
+++ CSE419/week 3/B.cpp
@@ -4,15 +4,15 @@ int main()
 {
     int n, m, k;
     cin >> n >> m >> k;
-    int app[n];
-    int ap[m];
-    map<int, int> mp;
+    int app[n] = {90, 41, 20, 39, 49, 21, 35, 31, 74, 86};
+    int ap[m] = {14, 24, 24, 7, 82, 85, 82, 4, 60, 95};
+    // map<int, int> mp;
     for (int i = 0; i < n; i++)
     {
         int x;
         scanf("%d", &x);
         app[i] = x;
-        mp[x]++;
+        // mp[x]++;
     }
     for (int i = 0; i < m; i++)
     {
@@ -20,24 +20,43 @@ int main()
     }
     sort(app, app + n);
     sort(ap, ap + m);
-
-    int count = 0;
-    map<int, int>::iterator it = mp.begin();
-
-    int i = 0;
-    while (it != mp.end())
+    int i = 0, j = 0, count = 0;
+    while (i < n && j < m)
     {
-        if (i == m)
-            break;
-        if (it->first <= ap[i] + k && it->first >= ap[i] - k)
+        if (ap[j] <= app[i] + k && ap[j] >= app[i] - k)
         {
+            i++;
+            j++;
             count++;
         }
-
-        i++;
-        it++;
-        // printf("%d\n", count);
+        else if ((app[i] + k) < ap[j])
+        {
+            i++;
+        }
+        else if ((app[i] - k) > ap[j])
+        {
+            j++;
+        }
     }
+    printf("%d\n", count);
+
+    // int count = 0;
+    // map<int, int>::iterator it = mp.begin();
+
+    // int i = 0;
+    // while (it != mp.end())
+    // {
+    //     if (i == m)
+    //         break;
+    //     if (it->first <= ap[i] + k && it->first >= ap[i] - k)
+    //     {
+    //         count++;
+    //     }
+
+    //     i++;
+    //     it++;
+    //     // printf("%d\n", count);
+    // }
     // while (it != mp.end())
     // {
     //     cout << "Key: " << it->first
@@ -45,6 +64,6 @@ int main()
     //     it++;
     // }
     // for (int i = 0; i < m; i++)
-    printf("%d\n", count);
+
     return 0;
 }
